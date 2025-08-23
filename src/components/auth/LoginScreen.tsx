819f199bd69aa0e28dbe037cea13bbe64ff7ade9
SUPERBUNNY <dglove780@gmail.com>
2025-08-23 07:21:41 +0000

    can you show creator and trader @DG143 at login page right corder in neo

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -104,6 +104,10 @@ const LoginScreen: React.FC<LoginScreenProps> = ({ initialStatus, revocationReas
           </Message>
         )}
       </div>
+      <div className="fixed bottom-4 right-4 text-xs text-primary font-mono p-2 rounded animate-flicker" style={{ textShadow: '0 0 2px var(--primary)' }}>
+        <p>Creator: @DG143</p>
+        <p>Trader: @DG143</p>
+      </div>
     </div>
   );
 };
